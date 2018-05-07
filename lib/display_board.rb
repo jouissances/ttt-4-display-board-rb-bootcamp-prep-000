6508da36412b26f5dd82a703fef01e26f6b2ba23
Janice Darikho <janicedarikho@gmail.com>
2018-05-07 14:48:21 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,13 +2,17 @@
 # out the current state.
 
 board = [" "," "," "," "," "," "," "," "," "]
-base = [ " #{board[0]} | #{board[1]}  | #{board[2]} ",
-         "-----------",
-         " #{board[3]} | #{board[4]}  | #{board[5]} ",
-         "-----------",
-         " #{board[6]} | #{board[7]}  | #{board[8]} ", ]
+# base = [ " #{board[0]} | #{board[1]}  | #{board[2]} ",
+#         "-----------",
+#         " #{board[3]} | #{board[4]}  | #{board[5]} ",
+#         "-----------",
+#         " #{board[6]} | #{board[7]}  | #{board[8]} ", ]
 
 def display_board(board)
-  puts base
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end
 
